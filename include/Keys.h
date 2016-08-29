15d3111f8d20d9e1e01bf2c5509539f667df8ba3
ajanaliz <jchbbst@gmail.com>
2016-08-30 00:47:36 +0430

    official Initial Commit

---
+++ include/Keys.h
@@ -0,0 +1,16 @@
+#ifndef KEYS_H
+#define KEYS_H
+
+
+class Keys
+{
+    public:
+        Keys();
+        virtual ~Keys();
+
+    protected:
+
+    private:
+};
+
+#endif // KEYS_H
---
+++ src/Keys.cpp
@@ -0,0 +1,11 @@
+#include "Keys.h"
+
+Keys::Keys()
+{
+    //ctor
+}
+
+Keys::~Keys()
+{
+    //dtor
+}
